5bfff64ce55a4a9fe33748a42bdeac279ab59e08
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-13 21:13:29 +0700

    inisialisasi project

---
+++ Tubes1KPL_Kelompok7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tubes1KPL_Kelompok7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
